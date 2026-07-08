f3f1f9100e489513e200a053b49be0f1785db82f
lucaargolo <lucaargolo@gmail.com>
2026-07-08 02:53:40 +0100

    Enhance `Instance` class: Add `Side` attribute for better management and update constructor and methods accordingly.

--- api/src/main/java/dev/lucaargolo/nexo/api/feature/Instance.java
+++ api/src/main/java/dev/lucaargolo/nexo/api/feature/Instance.java
@@ -1,6 +1,7 @@
 package dev.lucaargolo.nexo.api.feature;
 
 import dev.lucaargolo.nexo.api.feature.data.NexoData;
+import dev.lucaargolo.nexo.api.util.Side;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,13 +15,20 @@ public final class Instance<T extends Feature<T>> {
 
     @Nullable
     private final T feature;
+    @NotNull
+    private final Side side;
 
-    public Instance(@Nullable T feature) {
+    public Instance(@Nullable T feature, @NotNull Side side) {
         this.feature = feature;
+        this.side = side;
     }
 
     public @Nullable T get() {
         return feature;
+    }
+
+    public @NotNull Side side() {
+        return side;
     }
 
     @SuppressWarnings("unchecked")
@@ -32,5 +40,6 @@ public final class Instance<T extends Feature<T>> {
     public <D> void setData(@NotNull NexoData<D> data, @Nullable D d) {
         dataMap.put(data, d);
     }
+
 
 }
