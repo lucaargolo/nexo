6ad8d1a26f1d5954a46f67fded2e74ce0b5a4076
lucaargolo <lucaargolo@gmail.com>
2026-07-16 16:51:35 +0100

    Make Graphics3D extend Graphics2D: inherit 2D state stack, color, blend, line width, texture binding, and drawing primitives (rects, circles, ellipses, arcs, polygons, text, texture blitting, clip/scissor), keep 3D overloads for   translate/rotate/scale/drawLine with z/axis parameters, retain 3D-only depth/cull/normal/lightmap/camera/vertex submission methods

--- api/src/main/java/dev/lucaargolo/nexo/api/render/Graphics3D.java
+++ api/src/main/java/dev/lucaargolo/nexo/api/render/Graphics3D.java
@@ -1,37 +1,18 @@
 package dev.lucaargolo.nexo.api.render;
 
-import dev.lucaargolo.nexo.api.render.util.BlendMode;
 import dev.lucaargolo.nexo.api.render.util.CullMode;
 import dev.lucaargolo.nexo.api.render.util.DepthMode;
 import dev.lucaargolo.nexo.api.render.util.PrimitiveType;
-import dev.lucaargolo.nexo.api.render.util.TextureFilter;
-import dev.lucaargolo.nexo.api.render.util.TextureWrap;
 import dev.lucaargolo.nexo.api.render.util.VertexFormat;
-import dev.lucaargolo.nexo.api.util.Location;
 import org.jetbrains.annotations.NotNull;
-import org.joml.Matrix4f;
 import org.joml.Vector3f;
 
-public interface Graphics3D {
-
-    void pushMatrix();
-    void popMatrix();
-    void pushState();
-    void popState();
+public interface Graphics3D extends Graphics2D {
 
     void translate(float x, float y, float z);
     void rotate(float angle, float axisX, float axisY, float axisZ);
     void rotate(@NotNull Vector3f axis, float angle);
     void scale(float x, float y, float z);
-    void mulMatrix(@NotNull Matrix4f matrix);
-    @NotNull Matrix4f matrix();
-
-    void color(float r, float g, float b, float a);
-    void color(float @NotNull [] rgba);
-    float @NotNull [] color();
-
-    void blendMode(@NotNull BlendMode mode);
-    @NotNull BlendMode blendMode();
 
     void depthMode(@NotNull DepthMode mode);
     @NotNull DepthMode depthMode();
@@ -44,9 +25,6 @@ public interface Graphics3D {
     void normal(float nx, float ny, float nz);
     void normal(@NotNull Vector3f normal);
 
-    void lineWidth(float width);
-    float lineWidth();
-
     void drawLine(float x1, float y1, float z1, float x2, float y2, float z2);
     void drawCube(float x, float y, float z, float sizeX, float sizeY, float sizeZ);
     void drawQuad(@NotNull Vector3f v0, @NotNull Vector3f v1,
@@ -55,10 +33,6 @@ public interface Graphics3D {
     void begin(@NotNull PrimitiveType type, @NotNull VertexFormat format);
     void vertex(float @NotNull ... data);
     void end();
-
-    void bindTexture(@NotNull Location texture);
-    void textureFilter(@NotNull TextureFilter min, @NotNull TextureFilter mag);
-    void textureWrap(@NotNull TextureWrap wrapS, @NotNull TextureWrap wrapT);
 
     void perspective(float fov, float aspect, float near, float far);
     void ortho(float left, float right, float bottom, float top, float near, float far);
